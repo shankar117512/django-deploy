d1851685bb4e2b5435b0ecdadb25d64ab15df09a
shankar117512 <shankar117512@gmail.com>
2026-02-07 11:11:15 +0530

    update CSRF

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS= ["https://django-deploy-production-15a6.up.railway.app"]
+CSRF_TRUSTED_ORIGINS= ["https://django-deploy-production-c1c4.up.railway.app"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
